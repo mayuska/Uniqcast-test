aae264b1cd45bf9fa421bf001fd14a9b8c906afa
Maja Radošević <maja.radoshevic@gmail.com>
2017-04-08 10:43:01 +0200

    test completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ChannelsComponent } from './create-channel/create-channel.component';
+import { TableComponent } from './table/table.component';
+import { UpdateChannelComponent } from './update-channel/update-channel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelsComponent,
+    TableComponent,
+    UpdateChannelComponent
   ],
   imports: [
     BrowserModule,
